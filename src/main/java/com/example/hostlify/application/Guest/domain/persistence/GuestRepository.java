25417ae624df9584df6c21e9b8f5082c68807e85
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-17 22:43:15 -0500

    (feat):Guest add repository and service

--- src/main/java/com/example/hostlify/application/Guest/domain/persistence/GuestRepository.java
+++ src/main/java/com/example/hostlify/application/Guest/domain/persistence/GuestRepository.java
@@ -1,4 +1,13 @@
 package com.example.hostlify.application.Guest.domain.persistence;
 
-public interface GuestRepository {
+import com.example.hostlify.application.Guest.domain.model.Guest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestRepository extends JpaRepository<Guest,Long> {
+
+    Guest findByName(String name);
+    Guest findByEmail(String email);
+    Guest findByLastName(String lastName);
+
+
 }
--- src/main/java/com/example/hostlify/application/Guest/domain/service/GuestService.java
+++ src/main/java/com/example/hostlify/application/Guest/domain/service/GuestService.java
@@ -1,4 +1,15 @@
 package com.example.hostlify.application.Guest.domain.service;
 
+import com.example.hostlify.application.Guest.domain.model.Guest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface GuestService {
+    List<Guest> getAll();
+    Page<Guest> getAll(Pageable pageable);
+    Guest getByEmail(String email);
+    Guest getByName(String name);
+    Guest getByLastName(String lastName);
 }
