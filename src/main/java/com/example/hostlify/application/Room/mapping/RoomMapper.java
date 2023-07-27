eb61429c48f2ee93e27c6c1a8d64ba2e3468af83
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-26 23:17:22 -0500

    (feat):Room add mapping

--- src/main/java/com/example/hostlify/application/Room/mapping/RoomMapper.java
+++ src/main/java/com/example/hostlify/application/Room/mapping/RoomMapper.java
@@ -1,4 +1,33 @@
 package com.example.hostlify.application.Room.mapping;
 
-public class RoomMapper {
+import com.example.hostlify.application.Room.domain.model.Room;
+import com.example.hostlify.application.Room.resource.CreateRoomResource;
+import com.example.hostlify.application.Room.resource.RoomResource;
+import com.example.hostlify.application.Room.resource.UpdateRoomResource;
+import com.example.hostlify.shared.mapping.EnhancedModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Component
+@EnableAutoConfiguration
+public class RoomMapper implements Serializable {
+    @Autowired
+    EnhancedModelMapper mapper;
+
+    public RoomResource toResource(Room model) { return mapper.map(model, RoomResource.class); }
+
+    public Room toModel(CreateRoomResource resource) { return mapper.map(resource, Room.class); }
+
+    public Room toModel(UpdateRoomResource resource) { return mapper.map(resource, Room.class); }
+
+    public Page<RoomResource> modelListPage(List<Room> modelList, Pageable pageable){
+        return new PageImpl<>(mapper.mapList(modelList, RoomResource.class), pageable, modelList.size());
+    }
 }
