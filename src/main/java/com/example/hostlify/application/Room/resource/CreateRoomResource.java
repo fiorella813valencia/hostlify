2f3a77dbfc7dddeb8db28aaaeeca9ad34a5da4ee
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-26 23:49:34 -0500

    (feat):Room add create and update resource

--- src/main/java/com/example/hostlify/application/Room/resource/CreateRoomResource.java
+++ src/main/java/com/example/hostlify/application/Room/resource/CreateRoomResource.java
@@ -1,4 +1,36 @@
 package com.example.hostlify.application.Room.resource;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateRoomResource {
+
+    @NotNull
+    private int roomNumber;
+    @NotBlank
+    private String type;
+    @NotBlank
+    private String description;
+    @NotNull
+    private int capacity;
+    @NotBlank
+    private String beds;
+    @NotNull
+    private Float rate;
+    @NotBlank
+    private boolean status;
+    @NotBlank
+    private String facilities;
+    @NotBlank
+    private int floor;
+    @NotBlank
+    private String photo;
+
 }
--- src/main/java/com/example/hostlify/application/Room/resource/UpdateRoomResource.java
+++ src/main/java/com/example/hostlify/application/Room/resource/UpdateRoomResource.java
@@ -1,4 +1,29 @@
 package com.example.hostlify.application.Room.resource;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class UpdateRoomResource {
+    private Long roomId;
+    @NotNull
+    private int roomNumber;
+    @NotBlank
+    private String type;
+    @NotBlank
+    private String description;
+    @NotNull
+    private int capacity;
+    @NotBlank
+    private String beds;
+    @NotNull
+    private Float rate;
+    @NotBlank
+    private boolean status;
+    @NotBlank
+    private String facilities;
+    @NotBlank
+    private int floor;
+    @NotBlank
+    private String photo;
+
 }
