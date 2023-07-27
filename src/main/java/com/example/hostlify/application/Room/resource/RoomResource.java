613d37c38c68abf531bb57fa9fae7d4f8f5908f2
fiorella813valencia <89022353+fiorella813valencia@users.noreply.github.com>
2023-07-26 23:47:33 -0500

    (feat):Room add roomResource

--- src/main/java/com/example/hostlify/application/Room/resource/RoomResource.java
+++ src/main/java/com/example/hostlify/application/Room/resource/RoomResource.java
@@ -1,4 +1,33 @@
 package com.example.hostlify.application.Room.resource;
 
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomResource {
+    private Long roomId;
+
+    private int roomNumber;
+
+    private String type;
+
+    private String description;
+
+    private int capacity;
+
+    private String beds;
+
+    private Float rate;
+
+    private boolean status;
+
+    private String facilities;
+
+    private int floor;
+
+    private String photo;
+
 }
